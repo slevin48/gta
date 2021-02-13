db555a904fe27ef424c6dbe8b02114d239568751
Yann Debray <ydebray@mathworks.com>
2021-02-13 11:48:05 +0100

    matlab read img and np arrays

--- save/save_numpy_datetime.py
+++ save/save_numpy_datetime.py
@@ -3,12 +3,15 @@ import cv2
 import os
 import pandas as pd
 
+training_dataset = "training_data-2021-02-12-4"
 
-l = os.listdir("training_data/training_data-2021-02-12-2")
+l = os.listdir("training_data/"+training_dataset)
 n = len(l)
 
+os.mkdir("timeline/"+training_dataset)
+
 for k in range(n):
-    training_data = np.load("training_data/training_data-2021-02-12-2/training_data-"+str(k+1)+".npy",allow_pickle=True) 
+    training_data = np.load("training_data/"+training_dataset+"/training_data-"+str(k+1)+".npy",allow_pickle=True) 
     time_array = []
 
     for i in range(len(training_data)):
@@ -16,4 +19,4 @@ for k in range(n):
         time_array.append(training_data[i][1])
 
     df = pd.DataFrame(time_array,columns=["time"])
-    df.to_csv("timeline/timeline"+str(k+1)+".csv")
+    df.to_csv("timeline/"+training_dataset+"/timeline"+str(k+1)+".csv")
--- save/save_numpy_image.py
+++ save/save_numpy_image.py
@@ -6,18 +6,21 @@ import numpy as np
 import cv2
 import os
 
+training_dataset = "training_data-2021-02-12-4"
 
-l = os.listdir("training_data/training_data-2021-02-12-2")
+l = os.listdir("training_data/"+training_dataset)
 n = len(l)
 
+os.mkdir("screenshots/"+training_dataset)
+
 for k in range(n):
-    training_data = np.load("training_data/training_data-2021-02-12-2/training_data-"+str(k+1)+".npy",allow_pickle=True) 
+    training_data = np.load("training_data/"+training_dataset+"/training_data-"+str(k+1)+".npy",allow_pickle=True) 
 
     img_array = []
 
-    os.mkdir("screenshots/screenshots"+str(k+1))
+    os.mkdir("screenshots/"+training_dataset+"/screenshots"+str(k+1))
 
     for i in range(len(training_data)):
         # print(img[0][0])
         img_array.append(training_data[i][0])
-        cv2.imwrite("screenshots/screenshots"+str(k+1)+"/training_data_"+str(i)+".jpg",training_data[i][0])
+        cv2.imwrite("screenshots/"+training_dataset+"/screenshots"+str(k+1)+"/training_data_"+str(i)+".jpg",training_data[i][0])
